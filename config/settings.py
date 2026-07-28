78277dac8e5da3a2ffe63bb295ecda443643a84d
Garde Tanz Verein <Empfang@rentem.de>
2026-07-28 13:12:46 +0200

    Revert "Navigations-Seitenleiste im Admin standardmaessig zugeklappt starten"
    This reverts commit f2b4434a0a2a5d3218e3b1c35250a340ebc825b7.

--- config/settings.py
+++ config/settings.py
@@ -66,9 +66,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # DIRS hat Vorrang vor APP_DIRS: noetig, damit unsere eigene
-        # admin/base_site.html Django-contrib.admin's gleichnamige Datei ueberschreibt.
-        'DIRS': [BASE_DIR / 'mitglieder' / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
